f070950e39efb58e8dcc27d46a33599201935736
조호준 <hojun@johojun-ui-MacBookAir.local>
2025-02-24 01:01:56 +0900

    threads - firebase_core, firebase_auth 패키지 추가

--- threads/lib/main.dart
+++ threads/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:threads/app.dart';
+import 'package:threads/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   GoRouter.optionURLReflectsImperativeAPIs = true;
 
   runApp(
